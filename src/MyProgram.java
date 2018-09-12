aac91f6bdbc91916fc154233279628ba3ba2908d
DESKTOP-Q1D3SV2\Matthew <mromleski@gmail.com>
2018-09-12 10:02:41 -0500

    An update to let me test branch merging on GitHub.

--- src/MyProgram.java
+++ src/MyProgram.java
@@ -9,11 +9,17 @@ public class MyProgram {
 	public static void main(String[] args) {
 		Student firstStudent = new Student();
 		Student secondStudent = new Student();
+		Student thirdStudent = new Student();
+		
 		firstStudent.id = 1;
 		firstStudent.firstName = "Marc";
+		
 		secondStudent.id = 2;
 		secondStudent.firstName = "Sophie";
 
+		thirdStudent.id = 3;
+		thirdStudent.firstName = "John";
+		
 		System.out.println("The student object referred to " +
 		"by the variable secondStudent has the first " +
 		"name: " + secondStudent.firstName);
