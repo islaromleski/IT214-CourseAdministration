3358e30163e261a0deaf65eccb9a07632864e126
DESKTOP-Q1D3SV2\Matthew <mromleski@gmail.com>
2018-10-02 12:44:44 -0500

    Update which introduced some constructors & added accessor methods for the Book class.

--- src/Book.java
+++ src/Book.java
@@ -4,11 +4,50 @@
 
 public class Book {
 	final static int MAX_AMOUNT_OF_PAGES = 500;
-	final static int MIN_AMOUNT_OF_PAGES = 50;
-	
-	String title;
+	final static int MIN_AMOUNT_OF_PAGES = 50;	
+	final static int DEFAULT_YEAR = 2014;
+	final String title;
+	final int releaseYear;
+	private int copiesSold;
+	private int nmbrOfPages;
 	String[] authors;
+
+	Book(String inputTitle) {
+		// Calls other constructor:
+		this(inputTitle, DEFAULT_YEAR, -999999, 0);
+	}
+
+	Book(String inputTitle, int rYear) {
+		// Calls other constructor:
+		this(inputTitle, rYear, -999999, 0);
+	}
+
+	Book(String inputTitle, int rYear, int newPages) {
+		// Calls other constructor:
+		this(inputTitle, rYear, newPages, 0);
+	}
+
+	Book(String inputTitle, int rYear, int newPages, int currentSold) {
+		this.title = inputTitle;
+		this.releaseYear = rYear;
+		this.setNmbrOfPages(newPages);
+		this.copiesSold = currentSold;
+	}
 	
-	int yearReleased, nrOfPages;
-	int copiesSold = 0;
+	int getCopiesSold() {
+		return this.copiesSold;
+	}
+	
+	void setCopiesSold(int newCopiesSold) {
+		this.copiesSold = newCopiesSold;
+	}
+	
+	int getNmbrOfPages() {
+		return this.nmbrOfPages;
+	}
+	
+	void setNmbrOfPages(int newNmbrOfPages) {
+		this.nmbrOfPages = newNmbrOfPages;
+	}
+	
 }
--- src/MyProgram.java
+++ src/MyProgram.java
@@ -21,46 +21,42 @@ public class MyProgram {
 		thirdStudent.firstName = "John";
 		
 		System.out.println("The student object referred to " +
-		"by the variable secondStudent has the first " +
-		"name: " + secondStudent.firstName);
+		"by the variable secondStudent has a first " +
+		"name of " + secondStudent.firstName + ".\n");
 		
 		Course IT214 = new Course();
 		
 		IT214.setCourseID(214);
 		IT214.setCourseName("Fundamentals of Software Programming");
 		
-		Book book1 = new Book();
-		book1.title = "Beginning Java";
+		Book book1 = new Book("Beginning Java");
 		book1.authors = new String[] {
 			"Bart Baesens",
 			"Aimee Backiel",
 			"Seppe vanden Broucke"
 		};
 		
-		Book book2 = new Book();
-		book2.title = "Catcher in the Rye";
+		Book book2 = new Book("Catcher in the Rye");
 		book2.authors = new String[]{"J. D. Salinger"};
 		
-		Book book3 = new Book();
-		System.out.println("The title of the book is " + book3.title);
+		Book book3 = new Book("N/A");
+		System.out.println("The title of the book3 is " + book3.title + ".\n");
 		
 		Book book = null;
-		System.out.println("Now, book equals: " + book);
-		book = new Book();
-		System.out.println("And now, book equals: " + book);
+		System.out.println("First, book equals: " + book + ".");
+		book = new Book("N/A");
+		System.out.println("And now, book equals: " + book + ".\n");
 		
-		final Book superLargeBook = new Book();
-		superLargeBook.title = "Super Large Boring Book";
-		superLargeBook.nrOfPages = 1400;
+		final Book superLargeBook = new Book("Super Large Boring Book", 1947, 1400, 0);
 		
 		System.out.println("Check if your book has a valid number of pages!");
-		System.out.println("- Minimum amount: " + Book.MIN_AMOUNT_OF_PAGES);
-		System.out.println("- Maximum amount: " + Book.MAX_AMOUNT_OF_PAGES);
-		System.out.println("- Your book: " + superLargeBook.nrOfPages);
-		System.out.println("- Copies Sold: " + superLargeBook.copiesSold);
+		System.out.println("- Minimum amount: " + Book.MIN_AMOUNT_OF_PAGES + ".");
+		System.out.println("- Maximum amount: " + Book.MAX_AMOUNT_OF_PAGES + ".");
+		System.out.println("- Your book: " + superLargeBook.getNmbrOfPages() + ".");
+		System.out.println("- Copies Sold: " + superLargeBook.getCopiesSold() + ".");
 		
-		superLargeBook.copiesSold += 1000;
+		superLargeBook.setCopiesSold(superLargeBook.getCopiesSold()+1000);
 		
-		System.out.println("- Updated Copies Sold:" + superLargeBook.copiesSold);
+		System.out.println("- Updated Copies Sold: " + superLargeBook.getCopiesSold());
 	}
 }
